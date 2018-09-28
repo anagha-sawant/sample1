e2dab1681903a2e8ea98ac72771fb519e0e4474a
anagha-sawant <anghasawant7@gmail.com>
2018-09-28 14:58:03 +0530

    add account activation

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 gem 'faker', '~> 1.9', '>= 1.9.1'
 gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 gem 'bootstrap-will_paginate', '~> 1.0'
+gem 'rubysl-cgi', '~> 2.0', '>= 2.0.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
   def setup
     @user = users(:michael)
   end
@@ -35,19 +33,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
-  end
-
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
   end
 
   test "successful edit with friendly forwarding" do
